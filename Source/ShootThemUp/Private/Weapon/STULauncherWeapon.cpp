36ada25f533d602eac271dbe3ecad2e481bcc45e
Alexander Papchikhin <aleshmandr@gmail.com>
2021-06-01 00:22:14 +0200

    Spawning projectile

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -2,15 +2,19 @@
 
 
 #include "Weapon/STULauncherWeapon.h"
+#include "STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
 void ASTULauncherWeapon::StartFire()
 {
-}
-
-void ASTULauncherWeapon::StopFire()
-{
+	MakeShot();
 }
 
 void ASTULauncherWeapon::MakeShot()
 {
+	const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+	auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+
+
+	UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
 }
