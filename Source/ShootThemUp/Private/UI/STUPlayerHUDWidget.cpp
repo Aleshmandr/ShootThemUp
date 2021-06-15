37c51080a633c9437177c7f73c1f2d34792d4de1
Alexander Papchikhin <aleshmandr@gmail.com>
2021-06-15 22:36:49 +0200

    Spectating widget

--- Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
+++ Source/ShootThemUp/Private/UI/STUPlayerHUDWidget.cpp
@@ -8,36 +8,54 @@
 
 float USTUPlayerHUDWidget::GetNormalizedHealth() const
 {
-	const auto PlayerPawn = GetOwningPlayerPawn();
-	if (PlayerPawn == nullptr) { return 0.f; }
-	
-	const auto Component = PlayerPawn->GetComponentByClass(USTUHealthComponent::StaticClass());
-	const auto HealthComponent = Cast<USTUHealthComponent>(Component);
+	const auto HealthComponent = GetHealthComponent();
 	if (HealthComponent == nullptr) { return 0.f; }
-	
+
 	return HealthComponent->GetNormalizedHealth();
 }
 
 bool USTUPlayerHUDWidget::GetAmmoData(FAmmoData& AmmoData) const
 {
-	const auto PlayerPawn = GetOwningPlayerPawn();
-	if (PlayerPawn == nullptr) { return false; }
-	
-	const auto Component = PlayerPawn->GetComponentByClass(USTUWeaponComponent::StaticClass());
-	const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
+	const auto WeaponComponent = GetWeaponComponent();
 	if (WeaponComponent == nullptr) { return false; }
-	
+
 	return WeaponComponent->TryGetAmmoData(AmmoData);
 }
 
 bool USTUPlayerHUDWidget::TryGetWeaponUIData(FWeaponUIData& UIData) const
 {
-	const auto PlayerPawn = GetOwningPlayerPawn();
-	if (PlayerPawn == nullptr) { return false; }
-	
-	const auto Component = PlayerPawn->GetComponentByClass(USTUWeaponComponent::StaticClass());
-	const auto WeaponComponent = Cast<USTUWeaponComponent>(Component);
+	const auto WeaponComponent = GetWeaponComponent();
 	if (WeaponComponent == nullptr) { return false; }
-	
+
 	return WeaponComponent->TryGetWeaponUIData(UIData);
+}
+
+bool USTUPlayerHUDWidget::IsPlayerAlive() const
+{
+	const auto HealthComponent = GetHealthComponent();
+	return HealthComponent != nullptr && !HealthComponent->IsDead();
+}
+
+bool USTUPlayerHUDWidget::IsPlayerSpectating() const
+{
+	const auto Controller = GetOwningPlayer();
+	return Controller && Controller->GetStateName() == NAME_Spectating;
+}
+
+USTUWeaponComponent* USTUPlayerHUDWidget::GetWeaponComponent() const
+{
+	const auto PlayerPawn = GetOwningPlayerPawn();
+	if (PlayerPawn == nullptr) { return nullptr; }
+
+	const auto Component = PlayerPawn->GetComponentByClass(USTUWeaponComponent::StaticClass());
+	return Cast<USTUWeaponComponent>(Component);
+}
+
+USTUHealthComponent* USTUPlayerHUDWidget::GetHealthComponent() const
+{
+	const auto PlayerPawn = GetOwningPlayerPawn();
+	if (PlayerPawn == nullptr) { return nullptr; }
+
+	const auto Component = PlayerPawn->GetComponentByClass(USTUHealthComponent::StaticClass());
+	return Cast<USTUHealthComponent>(Component);
 }
--- Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
+++ Source/ShootThemUp/Public/UI/STUPlayerHUDWidget.h
@@ -8,9 +8,9 @@
 #include "Blueprint/UserWidget.h"
 #include "STUPlayerHUDWidget.generated.h"
 
-/**
- * 
- */
+class USTUWeaponComponent;
+class USTUHealthComponent;
+
 UCLASS()
 class SHOOTTHEMUP_API USTUPlayerHUDWidget : public UUserWidget
 {
@@ -22,4 +22,12 @@ class SHOOTTHEMUP_API USTUPlayerHUDWidget : public UUserWidget
 	bool GetAmmoData(FAmmoData& AmmoData) const;
 	UFUNCTION(BlueprintCallable, Category="UI")
 	bool TryGetWeaponUIData(FWeaponUIData& UIData) const;
+	UFUNCTION(BlueprintCallable, Category="UI")
+	bool IsPlayerAlive() const;
+	UFUNCTION(BlueprintCallable, Category="UI")
+	bool IsPlayerSpectating() const;
+	
+private:
+	USTUWeaponComponent* GetWeaponComponent() const;
+	USTUHealthComponent* GetHealthComponent() const;
 };
